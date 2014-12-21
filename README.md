63e0eba9b4a98fabc065b93c4de23cdc6817b9e8
Tom Boutell <tom@punkave.com>
2014-12-21 09:59:48 -0500

    up and running and analyzing our intranet logs

---
+++ README.md
@@ -0,0 +1,79 @@
+## Purpose
+
+You have Apache and Nginx-format log files, for private sites without Google Analytics. You'd like to know what the most popular URLs have been lately. You don't want to install a complex log analysis package. You just want a quick report, right now.
+
+## Install
+
+```
+npm install -g poptop
+```
+
+## Usage
+
+```
+tail -50000 /var/log/nginx/mysite.access.log | poptop
+```
+
+Out comes a list of URLs sorted by popularity.
+
+That list includes pages not found and other errors. Want to kick those out?
+
+```
+tail -50000 /var/log/nginx/mysite.access.log --successful | poptop
+```
+
+Want to focus on the "404 not found" errors?
+
+```
+tail -50000 /var/log/nginx/mysite.access.log --notfound | poptop
+```
+
+
+Reading the results in terminal? Want the most popular URLs at the bottom? No problem:
+
+```
+tail -50000 /var/log/nginx/mysite.access.log | poptop -r
+```
+
+That's great, but you don't care about static assets like `.gif`, `.png`, etc. OK, let's ignore them:
+
+```
+tail -50000 /var/log/nginx/mysite.access.log | poptop --ignore-static
+```
+
+Query strings are showing up as separate accesses. Let's clobber those:
+
+```
+tail -50000 /var/log/nginx/mysite.access.log | poptop --ignore-query
+```
+
+How about totals for folders?
+
+```
+tail -50000 /var/log/nginx/mysite.access.log | poptop --ignore-static --folders
+```
+
+Here's a really fancy command line from my actual life:
+
+tail -100000 mysite.access.log | node app --ignore-static --ignore-query --successful "--ignore=/admin|svn|/logout|/login" --folders
+
+## Search report
+
+After popular pages, the second most common question is "what did people search for?" Use the `--search` option to find out:
+
+tail -50000 /var/log/nginx/mysite.access.log | poptop --search=/search,q
+
+You'll need to specify the URL where searches happen, and the query string parameter that contains the search string, separated by a comma.
+
+(If your search engine uses POST requests, the search string won't be in the logs, so I can't help you there.)
+
+## Other options
+
+You can also ignore individual file extensions with `--ignore-extensions`. For convenience, `--ignore-static` is equivalent to `--ignore-extensions=gif,jpg,png,js,xlx,pptx,docx,css,ico,pdf`.
+
+And, you can ignore any regular expression you wish with the `--ignore` option. Be sure to quote it to avoid shell issues.
+
+## Credits
+
+`poptop` was created to facilitate our work at [P'unk Avenue](http://punkave.com).
+
---
+++ app.js
@@ -0,0 +1,142 @@
+/* jshint node:true */
+var argv = require('yargs').argv;
+var fs = require('fs');
+var byline = require('byline');
+var clf = require('clf-parser');
+var qs = require('qs');
+
+var ignore = argv.ignore ? [ argv.ignore ] : [];
+var ignoreExtensions = argv['ignore-extensions'] ? argv['ignore-extensions'] : [];
+
+if (argv.help) {
+  console.error('Usage: cat /var/log/nginx/my-logfile.log | poptop [--ignore-static] [--ignore-extensions=gif,png] [--ignore=regexp] [--ignore-query] [-r] [--successful] [--notfound] [--search=url,parameter]');
+  process.exit(1);
+}
+
+if (argv['ignore-static']) {
+  ignoreExtensions.push('gif,jpg,png,js,xlx,pptx,docx,css,ico,pdf');
+}
+
+var ignore;
+
+ignoreExtensions.forEach(function(list) {
+  ignore.push('\\.(' + list.replace(/,/g, '|') + ')$');
+});
+
+ignore = ignore.map(function(regexp) {
+  return new RegExp(regexp);
+});
+
+var popularity = {};
+
+var search = false;
+var searchUrl;
+var searchParameter;
+if (argv.search) {
+  search = true;
+  var components = (typeof(argv.search) === 'string') && argv.search.split(/,/);
+  if (components.length !== 2) {
+    console.error('The --search option looks like: --search=/search,q');
+    process.exit(1);
+  }
+  searchUrl = components[0];
+  searchParameter = components[1];
+}
+
+stream = byline.createStream(process.stdin);
+
+stream.on('data', function(line) {
+  var info = clf(line.toString('utf8'));
+  if (!info) {
+    return;
+  }
+  if (!info.path) {
+    return;
+  }
+  if (argv.successful) {
+    if ((info.status < 200) || (info.status > 399)) {
+      return;
+    }
+  }
+  if (argv.notfound) {
+    if (info.status !== 404) {
+      return;
+    }
+  }
+
+  if (search) {
+    if (info.path.indexOf(searchUrl) !== 0) {
+      return;
+    }
+    var q = info.path.indexOf('?');
+    if (q === -1) {
+      return;
+    }
+    var query = info.path.substr(q + 1);
+    query = qs.parse(query);
+    if (!query[searchParameter]) {
+      return;
+    }
+    var s = query[searchParameter];
+    s = s.toLowerCase();
+    if (!popularity[s]) {
+      popularity[s] = 0;
+    }
+    popularity[s]++;
+    return;
+  }
+
+  if (argv['ignore-query']) {
+    info.path = info.path.replace(/\?.*$/, '');
+  }
+  var i;
+  for (i = 0; (i < ignore.length); i++) {
+    if (info.path.match(ignore[i])) {
+      return;
+    }
+  }
+  var components;
+  var path;
+  var paths;
+  if (argv.folders && (info.path !== '/')) {
+    components = info.path.substr(1).split(/\//);
+    path = '';
+    paths = [];
+    components.forEach(function(c) {
+      path += '/' + c;
+      paths.push(path);
+    });
+  } else {
+    paths = [ info.path ];
+  }
+  paths.forEach(function(path) {
+    if (!popularity[path]) {
+      popularity[path] = 0;
+    }
+    popularity[path]++;
+  });
+});
+
+
+stream.on('end', function() {
+  var urls = Object.keys(popularity);
+  urls.sort(function(a, b) {
+    if (popularity[a] < popularity[b]) {
+      return 1;
+    }
+    if (popularity[a] > popularity[b]) {
+      return -1;
+    }
+    return 0;
+  });
+
+  if (argv.r) {
+    urls.reverse();
+  }
+
+  urls.forEach(function(url) {
+    console.log(popularity[url] + ': ' + url);
+  });
+});
+
+
